3658037f909079d5b81593199d3f9bc0939add01
victoria-parker <victoriaparkerb@gmail.com>
2022-09-08 19:56:28 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/* 
+Hacer un array con lo que esta en el blockquote
+
+Hacer un array con lo que esta en el input cuando termina el tiempo
+
+hacer una function que compare los arrays
+
+hacer una funcion que devuelva el puntaje en un alert y reemplace el texto del h4 con el resultado
+
+Hacer una funcion que controle el tiempo para el juego y que llame al resto de las funciones
+*/
+
+
+
+
+
+function startTimer(){
+
+}
+
+
+
+
+
+
+document.getElementById('playBtn').addEventListener('click',startTimer)
+
+/*     if(!timer){
+        timer=function(){
+            let sec=60;
+            let interval=setInterval(function(){
+                document.getElementById('timeLeft').innerHTML=sec;
+                sec--;
+                if(sec<0){
+                    clearInterval(timer)
+                }
+            },1000)
+        }
+    }
+ */
