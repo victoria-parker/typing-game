fbb55d27ec8a4f8e3ec78984b5771308da8c4a89
victoria-parker <victoriaparkerb@gmail.com>
2022-09-10 11:12:00 +0200

    compare input quote function done and give score

--- js/main.js
+++ js/main.js
@@ -14,28 +14,57 @@ function startTimer(){
     timeLeft.innerText=currentTime;
        
     let timerId=setInterval(()=>{
-        
+
         currentTime--;
         timeLeft.innerText=currentTime
-
+        
         if(currentTime === 0){
-            clearInterval(timerId)
+
+            finishGame(timerId)
+
         }
+
+
+
     },1000)
 }
 
+function finishGame(timerId){
+    clearInterval(timerId)
+    giveScore()
+    document.querySelector('textarea').disabled=true
+}
 
 
-/*     if(!timer){
-        timer=function(){
-            let sec=60;
-            let interval=setInterval(function(){
-                document.getElementById('timeLeft').innerHTML=sec;
-                sec--;
-                if(sec<0){
-                    clearInterval(timer)
-                }
-            },1000)
+/* compare quote and userInput */
+
+const quote=document.querySelector('blockquote').textContent
+
+function compareText(){
+    
+    let correct=0;
+
+    const userInput=document.querySelector('textarea').value.split('')
+    
+    const quoteArray=quote.split('')
+
+    for(let i=0;i<userInput.length;i++){
+        if(userInput[i]===quoteArray[i]){
+            correct++
         }
+
     }
- */
+
+    let score= Math.round((correct * 100) /quoteArray.length)
+    
+    return score
+}
+
+/* GIVE THE SCORE */
+
+function giveScore(){
+
+    let score=compareText();
+    document.querySelector('h4').innerText=`Your score is: ${score}`
+    document.querySelector('h4').style.color='salmon'
+}
