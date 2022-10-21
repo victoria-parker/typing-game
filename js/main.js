fa315d9ad8e025df51634f89755e0ee178f88bff
victoria-parker <victoriaparkerb@gmail.com>
2022-10-21 19:49:04 +0200

    quote api added

--- js/main.js
+++ js/main.js
@@ -35,10 +35,24 @@ function finishGame(timerId){
     document.querySelector('textarea').disabled=true
 }
 
+/* get quote */
+
+const quote=document.querySelector('blockquote')
+const cite=document.querySelector('cite')
+
+fetch('https://api.quotable.io/random')
+.then(res=>res.json())
+.then(data=>{
+    quote.innerText=data.content
+    cite.innerText=data.author
+})
+.catch(error=>{
+    console.error(error)
+    quote.innerText='You have power over your mind - not outside events. Realize this, and you will find strength.'
+    cite.innerText='Marcus Aurelius'
+})
 
 /* compare quote and userInput */
-
-const quote=document.querySelector('blockquote').textContent
 
 function compareText(){
     
@@ -46,7 +60,7 @@ function compareText(){
 
     const userInput=document.querySelector('textarea').value.split('')
     
-    const quoteArray=quote.split('')
+    const quoteArray=quote.textContent.split('')
 
     for(let i=0;i<userInput.length;i++){
         if(userInput[i]===quoteArray[i]){
@@ -65,6 +79,6 @@ function compareText(){
 function giveScore(){
 
     let score=compareText();
-    document.querySelector('h4').innerText=`Your score is: ${score}`
+    document.querySelector('h4').innerText=`Your score is: ${score}%`
     document.querySelector('h4').style.color='salmon'
 }
